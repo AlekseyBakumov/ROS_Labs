a2e310ce9f87817bf93e3119ffaee67adec1f6bb
AlekseyB <a.bakumov@g.nsu.ru>
2025-09-12 16:05:07 +0700

    Added ex03 with my package

---
+++ Mod2/ex03/aleksey_bakumov_package/src/my_node.cpp
@@ -0,0 +1,13 @@
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+#include <cstdio>
+
+int main(int argc, char ** argv)
+{
+  (void) argc;
+  (void) argv;
+
+  printf("hello world aleksey_bakumov_package package\n");
+  return 0;
+}
